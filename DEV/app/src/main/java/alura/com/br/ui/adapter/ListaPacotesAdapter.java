f4d75e7c91547c7b547fabf4bde71315b0f5b082
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-07-12 19:12:08 -0300

    Realizando o processo de bind no adapter

--- DEV/app/src/main/java/alura/com/br/ui/adapter/ListaPacotesAdapter.java
+++ DEV/app/src/main/java/alura/com/br/ui/adapter/ListaPacotesAdapter.java
@@ -1,10 +1,14 @@
 package alura.com.br.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,6 +19,7 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
     private final List<Pacote> pacotes;
     private Context context;
+    private Drawable drawableImagemPacote;
 
     public ListaPacotesAdapter(List<Pacote> pacotes, Context context) {
 
@@ -42,6 +47,23 @@ public class ListaPacotesAdapter extends BaseAdapter {
     public View getView(int position, View view, ViewGroup parent) {
 
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(position);
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem()
+                , "drawable", context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + "dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText(pacote.getPreco().toString());
 
         return viewCriada;
     }
