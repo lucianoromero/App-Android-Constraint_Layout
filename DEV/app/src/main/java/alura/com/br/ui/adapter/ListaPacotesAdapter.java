a67ba893a65b4980dffeb04aaae1435b82ac3917
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-07-13 13:12:50 -0300

    Formatando os dias do pacote e Formatando o preço e inserindo o titulo

--- DEV/app/src/main/java/alura/com/br/ui/adapter/ListaPacotesAdapter.java
+++ DEV/app/src/main/java/alura/com/br/ui/adapter/ListaPacotesAdapter.java
@@ -10,7 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import alura.com.br.R;
 import alura.com.br.model.Pacote;
@@ -59,11 +63,26 @@ public class ListaPacotesAdapter extends BaseAdapter {
         Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
         imagem.setImageDrawable(drawableImagemPacote);
 
+
         TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        dias.setText(pacote.getDias() + "dias");
+        String diasEmTexto = "";
+        int quantidadeDeDias = pacote.getDias();
+        if (quantidadeDeDias > 1) {
+            diasEmTexto = quantidadeDeDias + " dias";
+        } else {
+            diasEmTexto = quantidadeDeDias + " dia";
+        }
+        dias.setText(diasEmTexto);
+
 
         TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        preco.setText(pacote.getPreco().toString());
+        BigDecimal precoDoPacote = pacote.getPreco();
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(
+                new Locale("pt", "br"));
+        String moedaBrasileira = formatoBrasileiro
+                .format(precoDoPacote)
+                .replace("R$", "R$ ");
+        preco.setText(moedaBrasileira);
 
         return viewCriada;
     }
