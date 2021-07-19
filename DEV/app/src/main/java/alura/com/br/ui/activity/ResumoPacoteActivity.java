30353a67446abf104eac4dd132488d833f724da8
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-07-19 19:01:37 -0300

    Criando tela de resmumo de pacote e adicionando informacoes

---
+++ DEV/app/src/main/java/alura/com/br/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,16 @@
+package alura.com.br.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import alura.com.br.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
