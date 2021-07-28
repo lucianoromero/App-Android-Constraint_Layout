bc2a35823ec0a6c03a070b40d8eaa3389e7d7756
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-07-28 18:07:51 -0300

    Realizando o processo de bind

--- DEV/app/src/main/java/alura/com/br/ui/activity/ResumoPacoteActivity.java
+++ DEV/app/src/main/java/alura/com/br/ui/activity/ResumoPacoteActivity.java
@@ -2,9 +2,18 @@ package alura.com.br.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import alura.com.br.R;
+import alura.com.br.model.Pacote;
+import alura.com.br.util.DiasUtil;
+import alura.com.br.util.MoedaUtil;
+import alura.com.br.util.ResourceUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -12,5 +21,25 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
+                2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
