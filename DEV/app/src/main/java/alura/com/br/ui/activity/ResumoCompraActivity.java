08115182d92d212004faad5e183753799c45363c
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-08-24 15:25:53 -0300

    Realizando bind na tela de resumo da compra

--- DEV/app/src/main/java/alura/com/br/ui/activity/ResumoCompraActivity.java
+++ DEV/app/src/main/java/alura/com/br/ui/activity/ResumoCompraActivity.java
@@ -2,9 +2,18 @@ package alura.com.br.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import alura.com.br.R;
+import alura.com.br.model.Pacote;
+import alura.com.br.util.DataUtil;
+import alura.com.br.util.MoedaUtil;
+import alura.com.br.util.ResourceUtil;
 
 public class ResumoCompraActivity extends AppCompatActivity {
 
@@ -12,5 +21,27 @@ public class ResumoCompraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
+        setTitle("Resumo da compra");
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
+                2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil
+                .periodoEmTexto(pacoteSaoPaulo.getDias());
+        data.setText(periodoEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
