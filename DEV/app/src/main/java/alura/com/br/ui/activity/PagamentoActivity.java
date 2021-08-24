2d8dc9cd822765b8a9c6568212d4632d7b40f779
Romero <40298952+lucianoromero@users.noreply.github.com>
2021-08-24 10:47:01 -0300

    Limitando digitos com o digits e realizando o bind

--- DEV/app/src/main/java/alura/com/br/ui/activity/PagamentoActivity.java
+++ DEV/app/src/main/java/alura/com/br/ui/activity/PagamentoActivity.java
@@ -3,14 +3,35 @@ package alura.com.br.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import alura.com.br.R;
+import alura.com.br.model.Pacote;
+import alura.com.br.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
+                2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
